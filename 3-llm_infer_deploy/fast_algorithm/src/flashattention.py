f5e706156c7fd9753c0f9f1b047a64d78a4215ac
harleyszhang <zhg5200211@outlook.com>
2024-10-06 00:37:24 +0800

    feat: optimize flashattn_1 code

--- 3-llm_infer_deploy/fast_algorithm/src/flashattention.py
+++ 3-llm_infer_deploy/fast_algorithm/src/flashattention.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 import torch, math
 
 def normal_softmax(x):
-    L, L = x.shape
+    N, N = x.shape
     out = np.array(x)
-    for r in range(0, L):
+    for r in range(0, N):
         maxi = 0
-        for i in range(0, L):
+        for i in range(0, N):
             maxi = max(maxi, x[r, i])
         e_sum = 0
-        for i in range(0, L):
+        for i in range(0, N):
             e_sum += np.exp(x[r, i] - maxi)
-        for i in range(0, L):
+        for i in range(0, N):
             out[r, i] = np.exp(x[r, i] - maxi) / e_sum
     return out
 
@@ -28,22 +28,22 @@ def np_selfattn(q, k, v):
     return o
 
 def normal_selfattn(q, k, v):
-    L, Dim = q.shape
-    # [L, Dim] * [Dim, L] -> [L, L]
-    x = np.zeros([L, L], "float32")
-    for r in range(0, L):
-        for i in range(0, L):
+    N, Dim = q.shape
+    # [N, Dim] * [Dim, N] -> [N, N]
+    x = np.zeros([N, N], "float32")
+    for r in range(0, N):
+        for i in range(0, N):
             for j in range(0, Dim):
                 x[r, i] += q[r, j] * k[i, j]
 
-    # [L, L]
+    # [N, N]
     softmax = np_softmax(x)
 
-    # [L, L] * [L, Dim] -> [L, Dim]
-    o = np.zeros([L, Dim], "float32")
-    for r in range(0, L):
+    # [N, N] * [N, Dim] -> [N, Dim]
+    o = np.zeros([N, Dim], "float32")
+    for r in range(0, N):
         for i in range(0, Dim):
-            for j in range(0, L):
+            for j in range(0, N):
                 o[r, i] += softmax[r, j] * v[j, i]
 
     return o
@@ -98,24 +98,23 @@ def flashattn_1(Q, K, V):
     O = np.zeros([N, Dim], "float32")
     m = np.zeros([N],  "float32")
     d = np.zeros([N],  "float32")
-    
+                
     for r in range(0, N):
         # 计算 QK^T 的第 i 行结果 S[r][i]
         for i in range(0, N):
+            # QK^T
             for j in range(0, Dim):
                 S[r][i] += Q[r][j] * K[i][j] # K^T 的列就是 K 的行
-        
-        # [N,N] -> [N,N]; [N, N] * [N, Dim] -> [N, dim]
-        mm = S[r][0]
-        dd = 1
-        m[0] = mm
-        d[0] = dd
-    
-        for i in range(1, N):
+            
+            # softmax: [N,N] -> [N,N]
+            if i == 0:
+                mm = S[r][0]
+                dd = 0
             mm, dd = online_softmax_update(mm, dd, S[r][i], 1) # flops 为 6
             m[i] = mm
             d[i] = dd
         
+        # PV: [N, N] * [N, Dim] -> [N, dim]
         for c in range(0, Dim):
             o = 0
             for i in range(0, N):
@@ -133,28 +132,10 @@ def flashattn_1(Q, K, V):
             O[r][c] = o
       
     return O
-      
-L = 64
-Dim = 16
-BLK = 4
-MIN_M = -10000
 
-q = np.random.uniform(-1, 1, [L, Dim]).astype("float32")
-k = np.random.uniform(-1, 1, [L, Dim]).astype("float32")
-v = np.random.uniform(-1, 1, [L, Dim]).astype("float32")
-
-np_attn_res = np_selfattn(q, k, v)
-normal_selfattn_res = normal_selfattn(q, k, v)
-flashattn_0_res = flashattn_0(q, k, v)
-flashattn_1_res = flashattn_1(q, k, v)
-
-np.testing.assert_allclose(np_attn_res, normal_selfattn_res, 1e-6, 1e-6)
-print(np.testing.assert_allclose(np_attn_res, flashattn_0_res, 1e-6, 1e-6))
-print(np.testing.assert_allclose(np_attn_res, flashattn_1_res, 1e-6, 1e-6))
-
-class ScaleDotProductAttention(nn.Module):
+class SelfAttention(nn.Module):
     def __init__(self, ):
-        super(ScaleDotProductAttention, self).__init__()
+        super(SelfAttention, self).__init__()
         self.softmax = nn.Softmax(dim = -1)
 
     def forward(self, Q, K, V, mask=None):
@@ -163,35 +144,45 @@ class ScaleDotProductAttention(nn.Module):
         # 1, 计算 Q, K^T 矩阵的点积.(再除以 sqrt(d_k) 得到注意力分数矩阵，暂时忽略)
         scores = torch.matmul(Q, K_T)
         # 2, 如果有掩码，则将注意力分数矩阵中对应掩码位置的值设为负无穷大
-        # if mask is not None:
-        #     scores = scores.masked_fill(mask == 0, -1e9)
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
         # 3, 对注意力分数矩阵按照最后一个维度进行 softmax 操作，得到注意力权重矩阵，值范围为 [0, 1]
         attn_weights = self.softmax(scores)
         # 4, 将注意力权重矩阵乘以 V，得到最终的输出矩阵
         output = torch.matmul(attn_weights, V)
 
         return output
+    
+if __name__ == "__main__":
+       
+    N = 1024
+    Dim = 128
+    BLK = 4
+    MIN_M = -10000
 
-# 创建 Q、K、V 三个张量
-Q = torch.randn(100, 64, dtype=torch.float)  # (sequence_length, d_k)
-K = torch.randn(100, 64, dtype=torch.float)  # (sequence_length, d_k)
-V = torch.randn(100, 64, dtype=torch.float)  # (sequence_length, d_k)
+    Q = np.random.uniform(-1, 1, [N, Dim]).astype("float32")
+    K = np.random.uniform(-1, 1, [N, Dim]).astype("float32")
+    V = np.random.uniform(-1, 1, [N, Dim]).astype("float32")
 
-# 创建 ScaleDotProductAttention 层
-attention = ScaleDotProductAttention()
+    np_attn_res = np_selfattn(Q, K, V)
+    normal_selfattn_res = normal_selfattn(Q, K, V)
+    flashattn_0_res = flashattn_0(Q, K, V)
+    flashattn_1_res = flashattn_1(Q, K, V)
 
-# 将 Q、K、V 三个张量传递给 ScaleDotProductAttention 层进行计算
-pytorch_attention_output = attention(Q, K, V)
-output = flashattn_0(Q.numpy(), K.numpy(), V.numpy())
+    np.testing.assert_allclose(np_attn_res, normal_selfattn_res, 1e-6, 1e-6)
+    np.testing.assert_allclose(np_attn_res, flashattn_0_res, 1e-6, 1e-6)
+    np.testing.assert_allclose(np_attn_res, flashattn_1_res, 1e-6, 1e-6)
+    
+    attention = SelfAttention() # 创建 ScaleDotProductAttention 层
+    pytorch_attention_output = attention(torch.tensor(Q), torch.tensor(K), torch.tensor(V)) # 将 Q、K、V 三个张量传递给 ScaleDotProductAttention 层进行计算
+   
+    # 打印输出矩阵和注意力权重矩阵的形状
+    print(f"standard attention output shape: {pytorch_attention_output.shape}") 
+    print(f"flashattn_0 output shape: {flashattn_0_res.shape}")
 
-print(pytorch_attention_output[78][34], output[78][34])
-# 打印输出矩阵和注意力权重矩阵的形状
-print(f"standard attention output shape: {pytorch_attention_output.shape}") 
-print(f"flashattn_0 output shape: {output.shape}")
+    # print(f"attn_weights shape: {attn_weights.shape}") # torch.Size([5, 10, 10])
 
-# print(f"attn_weights shape: {attn_weights.shape}") # torch.Size([5, 10, 10])
-
-if torch.allclose(pytorch_attention_output, torch.tensor(output, dtype=torch.float), atol=1e-2):
-    print("flashattention0 与 PyTorch 标准 attention 结果一致!")
-else:
-    print("flashattention0 与 PyTorch 标准 attention结果不一致!")
+    if torch.allclose(pytorch_attention_output, torch.tensor(flashattn_1_res, dtype=torch.float), atol=1e-2):
+        print("flashattention0 与 PyTorch 标准 attention 结果一致!")
+    else:
+        print("flashattention1 与 PyTorch 标准 attention结果不一致!")
